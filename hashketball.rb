cebf9c7f50c52f93a3ede32e03e8812104fa916c
Patrick Liang <pl204511194@yahoo.com>
2022-02-22 19:14:04 -0500

    commit tree 5f78b45ea1bee60160756c49ad394c744d05206e

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,112 @@ def game_hash
 end
 
 # Write code here
+
+# def iteration
+#   game_hash.map do |location, teams|
+#     teams[:players].map do |stats|
+#       stats[:slam_dunks]
+#     end
+#   end
+# end
+
+# puts iteration
+
+def all_players
+  game_hash[:home][:players] + game_hash[:away][:players]
+end
+
+def player_stats player_name
+  all_players.find do |players|
+    players[:player_name] == player_name
+  end
+end
+
+def num_points_scored player_name
+  player_stats(player_name)[:points]
+end
+
+def shoe_size player_name
+  player_stats(player_name)[:shoe]
+end
+
+def find_team team_name
+  team_info = game_hash.find do |location, teams|
+    teams[:team_name] == team_name
+  end
+  team_info[1]
+end
+
+def team_colors team_name
+  team = find_team(team_name)
+  team[:colors]
+end
+
+def team_names
+  game_hash.map do |location, teams|
+    teams[:team_name]
+  end
+end
+
+def player_numbers team_name
+  team = find_team(team_name)
+  team[:players].map do |stats|
+    stats[:number]
+  end
+end
+
+def big_shoe_player
+  all_players.max_by do |player|
+    player[:shoe]
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe_player[:rebounds]
+end
+
+def most_points_scored
+  player_most_points = all_players.max_by do |player|
+    player[:points]
+  end
+  player_most_points[:player_name]
+end
+
+def winning_team
+  home_scores = game_hash[:home][:players].map do |stats|
+    stats[:points]
+  end
+  home_total_score = home_scores.sum
+  away_scores = game_hash[:away][:players].map do |stats|
+    stats[:points]
+  end
+  away_total_score = away_scores.sum
+  if home_total_score > away_total_score
+    game_hash[:home][:team_name]
+  else game_hash[:away][:team_name]
+  end
+end
+
+def player_with_longest_name
+  player_longest_name = all_players.max_by do |player|
+    player[:player_name].length
+  end
+  player_longest_name[:player_name]
+end
+
+def long_name_steals_a_ton?
+  player_longest_name = all_players.max_by do |player|
+    player[:player_name].length
+  end
+  long_name_steals = player_longest_name[:steals]
+  steals_array = all_players.map do |stats|
+    stats[:steals]
+  end
+  find_steals = steals_array.find do |steals|
+    long_name_steals < steals
+  end
+  if find_steals
+    false
+  else true
+  end
+end
